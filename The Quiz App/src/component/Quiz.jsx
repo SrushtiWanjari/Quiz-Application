43552212432be19991829403ef50899f3a7dbd63
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-09 15:35:21 +0530

    color theme added

--- The Quiz App/src/component/Quiz.jsx
+++ The Quiz App/src/component/Quiz.jsx
@@ -26,19 +26,19 @@ function Quiz() {
     setShowScore(false);
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
-      <div className="bg-white p-8 rounded-lg shadow-xl w-full max-w-lg text-center">
+    <div className="flex items-center justify-center min-h-screen bg-amber-100 p-4">
+      <div className="bg-amber-500 p-8 rounded-lg shadow-xl w-full max-w-lg text-center">
         {showScore ? (
           <div className="flex flex-col items-center justify-center">
-            <h2 className="text-3xl font-bold mb-4 text-lime-600">
+            <h2 className="text-3xl font-bold mb-5 text-amber-900">
               Quiz Complete!
             </h2>
-            <p className="text-xl text-gray-700 mb-8">
+            <p className="text-2xl font-bold text-white mb-8">
               You scored {score} out of {questions.length}.
             </p>
             <button
               onClick={handleRestartQuiz}
-              className="bg-lime-500 text-white font-bold py-3 px-8 rounded-full shadow-lg hover:bg-lime-600 transition duration-300 mb-4"
+              className="bg-amber-800 text-white font-bold py-3 px-8 rounded-full shadow-lg hover:bg-amber-700 transition duration-300 mb-4"
             >
               Restart Quiz
             </button>
@@ -46,10 +46,10 @@ function Quiz() {
         ) : (
           <div className="flex flex-col">
             <div className="mb-8">
-              <h3 className="text-2xl font-semibold mb-2 text-orange-600">
+              <h3 className="text-3xl font-bold mb-5 text-amber-900">
                 Question {currentQuestion + 1}/{questions.length}
               </h3>
-              <p className="text-lg text-gray-800">
+              <p className="text-xl font-semibold text-grey-700">
                 {questions[currentQuestion].questionText}
               </p>
             </div>
