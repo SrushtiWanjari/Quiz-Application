969cb66aa6f0d99d382fe123110f611d6921b731
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-09 14:46:05 +0530

    Quiz page added

---
+++ The Quiz App/src/config/Info.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+      questionText: 'What is the primary language for building web browser user interfaces?',
+      answerOptions: [
+        { answerText: 'Python', isCorrect: false },
+        { answerText: 'JavaScript', isCorrect: true },
+        { answerText: 'Java', isCorrect: false },
+        { answerText: 'C++', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Which CSS property is used for controlling the space between elements?',
+      answerOptions: [
+        { answerText: 'padding', isCorrect: false },
+        { answerText: 'margin', isCorrect: true },
+        { answerText: 'border', isCorrect: false },
+        { answerText: 'height', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Which of the following is NOT a JavaScript framework?',
+      answerOptions: [
+        { answerText: 'React', isCorrect: false },
+        { answerText: 'Vue', isCorrect: false },
+        { answerText: 'Angular', isCorrect: false },
+        { answerText: 'Django', isCorrect: true },
+      ],
+    },
+    {
+      questionText: 'What does the `useState` hook return in React?',
+      answerOptions: [
+        { answerText: 'A boolean value', isCorrect: false },
+        { answerText: 'A stateful value and a function to update it', isCorrect: true },
+        { answerText: 'An array of components', isCorrect: false },
+        { answerText: 'A reference to a DOM element', isCorrect: false },
+      ],
+    },
+  ];
---
+++ The Quiz App/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import QuizPage from './component/Quiz.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <QuizPage/>
+)
