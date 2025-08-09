5fd58c4bd51b42acba9399f03d6505a120f920a8
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-08-09 14:59:03 +0530

    useState added

--- The Quiz App/src/component/Quiz.jsx
+++ The Quiz App/src/component/Quiz.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react-router";
+import questions from "../config/questions.js";
 
 function Quiz() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const handleRestartQuiz = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  };
+  return <div></div>;
 }
 
-export default Quiz
+export default Quiz;
